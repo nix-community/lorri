7c19a8567e9818e049818b0c1b54ca25c3e3ee93
Profpatsch <mail@profpatsch.de>
2024-04-19 15:01:53 +0200

    refact(tests/gc): repair the gc integration test
    What an amazing piece of code.
    
    Apparently it wasn’t even worth the work to remove all `dbg!` macros
    before submitting it upstream.
    
    Get rid of adding `stderr` to the checked output.
    
    I have a very trigger-happy finger that was very close to simply
    deleting this whole file without a second thought, but I suppose
    having some kind of integration test for this is at least a little
    useful.

--- tests/integration/gc.rs
+++ tests/integration/gc.rs
@@ -18,9 +18,7 @@ fn run_lorri<T: AsRef<std::ffi::OsStr>>(args: Vec<T>) -> std::io::Result<String>
         .args(args)
         .stdin(std::process::Stdio::null())
         .output()?;
-    let mut res = String::from_utf8(out.stdout).expect("non utf8 output");
-    res.push_str(std::str::from_utf8(&out.stderr).expect("non utf8 stderr"));
-    Ok(res)
+    Ok(String::from_utf8(out.stdout).expect("non utf8 output"))
 }
 
 #[test]
@@ -58,9 +56,7 @@ derivation {
     std::env::remove_var("XDG_CACHE_HOME");
     std::env::set_current_dir(&project).expect("cd");
     // build the project
-    assert!(dbg!(run_lorri(vec!["shell", "--shell-file", "shell.nix"]))
-        .expect("running lorri shell")
-        .contains("done"));
+    run_lorri(vec!["shell", "--shell-file", "shell.nix"]).expect("running lorri shell");
 
     //look for the gc root
     let pd = ProjectDirs::from("com.github.nix-community.lorri", "lorri", "lorri")
@@ -97,28 +93,26 @@ derivation {
     assert_eq!(out, "[]");
     // it should also not have removed it
     let out = run_lorri(vec!["gc", "info"]).unwrap();
-    assert!(dbg!(out).contains(&dbg!(subdir.display().to_string())));
+    assert!(out.contains(&subdir.display().to_string()));
     // Now remove the project
     let backup_file = project.join("shell.nix.bak");
     std::fs::rename(&nix_file, &backup_file).expect("rename");
     assert!(std::fs::metadata(&nix_file_symlink).is_err());
     // it should be labeled as dead, but not removed by --print-roots
     let out = run_lorri(vec!["gc", "info"]).unwrap();
-    assert!(dbg!(&out).contains(&dbg!(subdir.display().to_string())));
-    assert!(dbg!(out).contains("[dead]"));
+    assert!(&out.contains(&subdir.display().to_string()));
+    assert!(out.contains("[dead]"));
     // now remove it
     let out = run_lorri(vec!["gc", "--json", "rm"]).unwrap();
-    assert!(dbg!(&out).contains(&dbg!(subdir.display().to_string())));
+    assert!(&out.contains(&subdir.display().to_string()));
     let out = run_lorri(vec!["gc", "--json", "rm"]).unwrap();
     assert_eq!(out, "[]");
     // rebuild the project
     std::fs::rename(&backup_file, &nix_file).expect("rename back");
-    assert!(dbg!(run_lorri(vec!["shell", "--shell-file", "shell.nix"]))
-        .expect("running lorri shell")
-        .contains("done"));
+    run_lorri(vec!["shell", "--shell-file", "shell.nix"]).expect("running lorri shell");
     // everything back to normal
     let out = run_lorri(vec!["gc", "info"]).unwrap();
-    assert!(dbg!(&out).contains(&dbg!(subdir.display().to_string())));
-    assert!(dbg!(out).contains(&dbg!(nix_file.display().to_string())));
+    assert!(&out.contains(&subdir.display().to_string()));
+    assert!(out.contains(&nix_file.display().to_string()));
     Ok(())
 }
