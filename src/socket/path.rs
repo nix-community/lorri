63659a9c77fcb05a0fc6d98fcb3ca0ddc2f4debd
Profpatsch <mail@profpatsch.de>
2025-02-18 04:50:05 +0100

    refact(communicate): use new Flock auto-dropping impl of file lock
    This is kinda nice, let’s hope it works as I expect!

--- src/socket/path.rs
+++ src/socket/path.rs
@@ -3,8 +3,9 @@
 use crate::ops::error::{ExitAs, ExitErrorType};
 use crate::AbsPathBuf;
 use nix::fcntl;
+use nix::fcntl::Flock;
 use std::fmt;
-use std::os::unix::io::AsRawFd;
+use std::fs::File;
 use std::path::Path;
 use thiserror::Error;
 use tokio::net::{UnixListener, UnixStream};
@@ -45,10 +46,6 @@ impl From<std::io::Error> for BindError {
     }
 }
 
-/// Locks the socket the server is bound to. Drop to release.
-#[allow(dead_code)]
-pub struct BindLock(std::fs::File);
-
 impl SocketPath {
     /// Create from the path of the socket.
     /// Must be passed a valid socket file path (ending in a file name).
@@ -57,21 +54,21 @@ impl SocketPath {
     }
 
     /// Try to lock the lock file to find out whether another process is listening.
-    pub fn lock(&self) -> Result<BindLock, BindError> {
+    pub fn lock(&self) -> Result<Flock<File>, BindError> {
         let h = std::fs::OpenOptions::new()
             .read(true)
             .write(true)
             .create(true)
             .open(self.lockfile())?;
         // we try to get an exclusive lock, nonblocking
-        match fcntl::flock(h.as_raw_fd(), nix::fcntl::FlockArg::LockExclusiveNonblock) {
+        match fcntl::Flock::lock(h, nix::fcntl::FlockArg::LockExclusiveNonblock) {
             // if the lock would block, another process is listening
-            Err(nix::Error::EWOULDBLOCK) => Err(BindError::OtherProcessListening(
+            Err((_, nix::Error::EWOULDBLOCK)) => Err(BindError::OtherProcessListening(
                 self.lockfile().display().to_string(),
             )),
-            other => other.map_err(BindError::Unix),
-        }?;
-        Ok(BindLock(h))
+            Err((_, other)) => Err(BindError::Unix(other)),
+            Ok(a) => Ok(a),
+        }
     }
 
     /// `bind(2)` to this socket path.
@@ -83,7 +80,7 @@ impl SocketPath {
     ///
     /// Uses the `flock(2)` trick decribed in
     /// <https://gavv.github.io/articles/unix-socket-reuse/>
-    pub async fn bind(&self) -> Result<(UnixListener, BindLock), BindError> {
+    pub async fn bind(&self) -> Result<(UnixListener, Flock<File>), BindError> {
         // - try to lock lockfile (open and flock exclusive nonblocking)
         let lock = self.lock()?;
         // - remove socket file if it exists
