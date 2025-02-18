2a5b1cc44e2983bb4e65b0e1220e9e1ab1c0fa3a
Profpatsch <mail@profpatsch.de>
2025-02-18 20:17:19 +0100

    feat(main): improve crash error message
    If a panic happens, we now directly present a clickable link that
    opens an issue on Github, containing the error in the URL if it fits,
    or at least the title (and prompting the user to copy the error
    message themselves).
    
    This means we can get rid of the `human-panic` vendor, since I inlined
    the interesting bits (which are not all that many tbh, mainly the
    backtrace logic).

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,22 @@ use lorri::project::{Project, ProjectFile};
 use lorri::{constants, AbsPathBuf};
 use lorri::{logging, AbsDirPathBuf};
 use slog::{debug, error, o};
-use std::env;
+use std::fmt::Write as FmtWrite;
+use std::io::Write;
+use std::panic::PanicHookInfo;
 use std::path::Path;
+use std::{env, mem, panic};
 use structopt::StructOpt;
 
 use anyhow::anyhow;
+use backtrace::Backtrace;
+
 const TRIVIAL_SHELL_SRC: &str = include_str!("./trivial-shell.nix");
 const DEFAULT_ENVRC: &str = include_str!("./default-envrc");
 
 #[tokio::main]
 async fn main() {
-    install_panic_handler();
+    setup_panic();
 
     let exit_code = {
         let opts = Arguments::from_args();
@@ -43,22 +48,6 @@ async fn main() {
     };
 
     std::process::exit(exit_code);
-}
-
-#[allow(deprecated)]
-fn install_panic_handler() {
-    if let Err(env::VarError::NotPresent) = env::var("LORRI_NO_INSTALL_PANIC_HANDLER") {
-        // This returns 101 on panics, see also `ExitError::panic`.
-        human_panic::setup_panic!();
-    }
-}
-
-// Exit with return code 0 on SIGINT and SIGTERM
-fn install_signal_handler() {
-    ctrlc::set_handler(move || {
-        std::process::exit(0);
-    })
-    .expect("Error setting SIGINT and SIGTERM handler");
 }
 
 /// Search for `name` in the current directory.
@@ -117,7 +106,10 @@ async fn run_command(logger: &slog::Logger, opts: Arguments) -> Result<(), ExitE
             ops::op_watch(project, opts, &logger).await
         }
         Command::Daemon(opts) => {
-            install_signal_handler();
+            ctrlc::set_handler(move || {
+                std::process::exit(0);
+            })
+            .expect("Error setting SIGINT and SIGTERM handler");
             ops::op_daemon(opts, logger).await
         }
         Command::Init => ops::op_init(TRIVIAL_SHELL_SRC, DEFAULT_ENVRC, logger),
@@ -190,4 +182,194 @@ mod tests {
         path.push("this-lorri-specific-file-probably-does-not-exist");
         assert_eq!(None, is_file_in_current_directory(&path).unwrap());
     }
+}
+
+fn setup_panic() {
+    enum PanicStyle {
+        Debug,
+        Human,
+    }
+
+    let mut style = PanicStyle::Human;
+
+    // all these lead to “normal” rust panic output
+    if cfg!(debug_assertions) {
+        style = PanicStyle::Debug
+    }
+    if let Ok(_) = env::var("RUST_BACKTRACE") {
+        style = PanicStyle::Debug
+    };
+    if let Ok(_) = env::var("LORRI_NO_INSTALL_PANIC_HANDLER") {
+        style = PanicStyle::Debug
+    }
+    if let Ok(_) = env::var("RUST_BACKTRACE") {
+        style = PanicStyle::Debug
+    };
+
+    match style {
+        PanicStyle::Debug => {}
+        PanicStyle::Human => {
+            panic::set_hook(Box::new(move |info: &PanicHookInfo<'_>| {
+                let message = match (
+                    info.payload().downcast_ref::<&str>(),
+                    info.payload().downcast_ref::<String>(),
+                ) {
+                    (Some(s), _) => Some((*s).to_owned()),
+                    (_, Some(s)) => Some(s.to_owned()),
+                    (None, None) => None,
+                };
+
+                let cause = message.unwrap_or_else(|| "Unknown".into());
+
+                let cause_first = cause.lines().next();
+
+                let expl = match info.location() {
+                    Some(location) => format!(
+                        "Panic occurred in file '{}' at line {}\n",
+                        location.file(),
+                        location.line()
+                    ),
+                    None => "Panic location unknown.\n".to_string(),
+                };
+
+                let pos = info
+                    .location()
+                    .map(|location| format!("'{}', line {}\n: ", location.file(), location.line()));
+
+                let crate_version = env!("CARGO_PKG_VERSION");
+
+                let operating_system = os_info::get().to_string();
+                let backtrace = render_backtrace();
+
+                let title = format!(
+                    "Crash: {}{}",
+                    pos.unwrap_or("".to_string()),
+                    cause_first.unwrap_or("")
+                );
+                let body = format!(
+                    r##"# Crash Report
+
+<!-- Describe here what you did that lead to the crash -->
+## Crash Info
+
+* OS: {operating_system}
+* Lorri Version: {crate_version}
+
+{expl}
+Cause: {cause}
+
+Backtrace:
+```
+{backtrace}
+```
+"##
+                );
+
+                let issue_url = format!(
+                    "https://github.com/nix-community/lorri/issues/new?title={}&body={}",
+                    urlencoding::encode(&title),
+                    urlencoding::encode(&body)
+                );
+
+                let common = r##"lorri had a problem and crashed. To help us diagnose the problem you can send us a crash report.
+
+We take privacy seriously, and do not perform any automated error collection. In order to improve the software, we rely on people to submit reports.
+"##;
+
+                // if the URL would get longer than 2000 characters, we just add the title
+                // and let the user copy the error themselves.
+                let full_message = if issue_url.as_bytes().len() <= 2000 {
+                    format!(
+                        "{common}\nYou can submit a report to github by clicking on the following URL:\n\n{}\n",
+                        issue_url
+                    )
+                } else {
+                    format!(
+                        r##"{body}
+{common}
+You can submit a report to github but going to the following URL and pasting the error message before this text:
+https://github.com/nix-community/lorri/issues/new?title={}
+"##,
+                        urlencoding::encode(&title),
+                    )
+                };
+
+                let stderr = std::io::stderr();
+                let mut stderr = stderr.lock();
+                writeln!(
+                    &mut stderr,
+                    r##"{full_message}
+Thank you kindly!
+"##
+                )
+                .expect("printing error message to console failed");
+                drop(stderr);
+            }));
+        }
+    }
+}
+
+fn render_backtrace() -> String {
+    //We take padding for address and extra two letters
+    //to pad after index.
+    #[allow(unused_qualifications)] // needed for pre-1.80 MSRV
+    const HEX_WIDTH: usize = mem::size_of::<usize>() * 2 + 2;
+    //Padding for next lines after frame's address
+    const NEXT_SYMBOL_PADDING: usize = HEX_WIDTH + 6;
+
+    let mut backtrace = String::new();
+
+    //Here we iterate over backtrace frames
+    //(each corresponds to function's stack)
+    //We need to print its address
+    //and symbol(e.g. function name),
+    //if it is available
+    let bt = Backtrace::new();
+    let symbols = bt
+        .frames()
+        .iter()
+        .flat_map(|frame| {
+            let symbols = frame.symbols();
+            if symbols.is_empty() {
+                vec![(frame, None, "<unresolved>".to_owned())]
+            } else {
+                symbols
+                    .iter()
+                    .map(|s| {
+                        (
+                            frame,
+                            Some(s),
+                            s.name()
+                                .map(|n| n.to_string())
+                                .unwrap_or_else(|| "<unknown>".to_owned()),
+                        )
+                    })
+                    .collect::<Vec<_>>()
+            }
+        })
+        .collect::<Vec<_>>();
+    let begin_unwind = "rust_begin_unwind";
+    let begin_unwind_start = symbols
+        .iter()
+        .position(|(_, _, n)| n == begin_unwind)
+        .unwrap_or(0);
+    for (entry_idx, (frame, symbol, name)) in symbols.iter().skip(begin_unwind_start).enumerate() {
+        let ip = frame.ip();
+        let _ = writeln!(backtrace, "{entry_idx:4}: {ip:HEX_WIDTH$?} - {name}");
+        if let Some(symbol) = symbol {
+            //See if there is debug information with file name and line
+            if let (Some(file), Some(line)) = (symbol.filename(), symbol.lineno()) {
+                let _ = writeln!(
+                    backtrace,
+                    "{:3$}at {}:{}",
+                    "",
+                    file.display(),
+                    line,
+                    NEXT_SYMBOL_PADDING
+                );
+            }
+        }
+    }
+
+    backtrace
 }
